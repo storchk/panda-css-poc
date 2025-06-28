7e67eb5057cf667d032e86b810de6708ab841571
Kevin Storch <kevin.storch@mail.de>
2025-06-28 13:14:21 +0200

    refactor(tokens): use definePreset

--- src/styles/themes/shared/tokens.ts
+++ src/styles/themes/shared/tokens.ts
@@ -1,36 +1,38 @@
-/* eslint-disable @pandacss/no-config-function-in-source */
+import { definePreset } from '@pandacss/dev'
 
-import { defineTokens, defineSemanticTokens } from "@pandacss/dev";
-import type { Tokens, SemanticTokens } from "@pandacss/types";
 import { colors } from "./colors";
 import { spacing } from "./spacing";
 import { fontSizes } from "./font";
 
-export const tokens: Tokens = defineTokens({
-  colors,
-  spacing,
-  fontSizes,
-});
-
-export const semanticTokens: SemanticTokens = defineSemanticTokens({
-  colors: {
-    primary: {
-      value: {
-        base: "{colors.brand.primary}",
-        _dark: "{colors.brand.primary}",
-      },
-    },
-    secondary: {
-      value: {
-        base: "{colors.brand.secondary}",
-        _dark: "{colors.brand.secondary}",
-      },
-    },
-    tertiary: {
-      value: {
-        base: "{colors.grey.500}",
-        _dark: "{colors.grey.700}",
-      },
-    },
+export const preset = definePreset({
+  name: 'poc-preset',
+  theme: {
+    tokens: {
+      colors,
+      spacing,
+      fontSizes,
+    }
   },
-});
+  semanticTokens: {
+    colors: {
+      primary: {
+        value: {
+          base: "{colors.brand.primary}",
+          _dark: "{colors.brand.primary}",
+        },
+      },
+      secondary: {
+        value: {
+          base: "{colors.brand.secondary}",
+          _dark: "{colors.brand.secondary}",
+        },
+      },
+      tertiary: {
+        value: {
+          base: "{colors.grey.500}",
+          _dark: "{colors.grey.700}",
+        },
+      },
+    },
+  }
+})
